0937b76ad4ef717548ced213f618473c12240190
Ayush Goyal <ayush987goyal@gmail.com>
2018-03-20 02:27:08 +0530

    added array chunking

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,50 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const result = [];
+  let i = 0;
+
+  while (i < array.length) {
+    result.push(array.slice(i, i + size));
+    i += size;
+  }
+
+  return result;
+}
 
 module.exports = chunk;
+
+// function chunk(array, size) {
+//   let i = 0;
+//   let result = [];
+
+//   while (i < array.length) {
+//     let small = [];
+//     for (let j = i; j < i + size; j++) {
+//       if (!array[j]) break;
+
+//       small.push(array[j]);
+//     }
+//     result.push(small);
+//     i = i + size;
+//   }
+
+//   return result;
+// }
+
+// function chunk(array, size) {
+//   const result = [];
+
+//   for (let element of array) {
+//     const last = result[result.length - 1];
+
+//     if (!last || last.length === size) {
+//       result.push([element]);
+//     } else {
+//       result[result.length - 1].push(element);
+//     }
+//   }
+
+//   return result;
+// }
