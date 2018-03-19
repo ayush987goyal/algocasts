db1d36c65e0e4c741f3c5c0035e8ceb4eefb028f
Ayush Goyal <ayush987goyal@gmail.com>
2018-03-20 02:59:34 +0530

    added anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,61 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(str) {
+  return str
+    .replace(/[^\w]/g, '')
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('');
+}
 
 module.exports = anagrams;
+
+// function anagrams(stringA, stringB) {
+//   const strA = stringA.replace(/[^\w]/gi, '').toLowerCase();
+//   const arrB = stringB
+//     .replace(/[^\w]/gi, '')
+//     .toLowerCase()
+//     .split('');
+
+//   for (let letter of strA) {
+//     const index = arrB.indexOf(letter);
+//     if (index > -1) {
+//       arrB.splice(index, 1);
+//     }
+//   }
+
+//   return arrB.length === 0;
+// }
+
+// function anagrams(stringA, stringB) {
+//   const charMapA = makeCharMap(stringA);
+//   const charMapB = makeCharMap(stringB);
+
+//   if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+//     return false;
+//   }
+
+//   for (let letter in charMapA) {
+//     if (charMapA[letter] !== charMapB[letter]) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
+
+// function makeCharMap(str) {
+//   const obj = {};
+
+//   for (let letter of str.replace(/[^\w]/g, '').toLowerCase()) {
+//     obj[letter] = obj[letter] + 1 || 1;
+//   }
+
+//   return obj;
+// }
