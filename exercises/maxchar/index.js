a984319e6cd6934b756b1f1e87debc47e8b42ba0
Ayush Goyal <ayush987goyal@gmail.com>
2018-03-20 01:55:48 +0530

    added maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let maxValue = 0;
+  let result = '';
+  const obj = {};
+
+  for (const letter of str) {
+    // if (letter in obj) {
+    //   obj[letter] += 1;
+    // } else {
+    //   obj[letter] = 1;
+    // }
+    obj[letter] = obj[letter] + 1 || 1;
+
+    if (obj[letter] > maxValue) {
+      maxValue = obj[letter];
+      result = letter;
+    }
+  }
+
+  return result;
+}
 
 module.exports = maxChar;
