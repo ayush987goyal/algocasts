fba7e87c744e1a28b8db9c63142c8e41c9ea09fe
Ayush Goyal <ayush987goyal@gmail.com>
2018-03-20 23:43:43 +0530

    added pyramid, important

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,44 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, level = '') {
+  if (n === row) {
+    return;
+  }
+
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const midpoint = Math.floor((2 * n - 1) / 2);
+
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    level += '#';
+  } else {
+    level += ' ';
+  }
+  pyramid(n, row, level);
+}
 
 module.exports = pyramid;
+
+// function pyramid(n) {
+//   let i = 0;
+//   const midpoint = Math.floor((2 * n - 1) / 2);
+
+//   while (i < n) {
+//     let str = '';
+
+//     for (let j = 0; j < 2 * n - 1; j++) {
+//       if (midpoint - i <= j && midpoint + i >= j) {
+//         str += '#';
+//       } else {
+//         str += ' ';
+//       }
+//     }
+
+//     console.log(str);
+//     i++;
+//   }
+// }
