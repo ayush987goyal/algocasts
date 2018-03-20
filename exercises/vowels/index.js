2965267e5d9cb308652dbf4e3d6bd6d1f51ba3a1
Ayush Goyal <ayush987goyal@gmail.com>
2018-03-20 23:58:46 +0530

    added vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,36 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+
+  return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
+
+// function vowels(str) {
+//   const vols = ['a', 'e', 'i', 'o', 'u'];
+//   let count = 0;
+
+//   for (let letter of str.toLowerCase()) {
+//     count += vols.includes(letter) ? 1 : 0;
+//   }
+
+//   return count;
+// }
+
+// function vowels(str) {
+//   const vols = ['a', 'e', 'i', 'o', 'u'];
+//   return str
+//     .toLowerCase()
+//     .split('')
+//     .reduce((val, letter) => {
+//       let newVal = val;
+//       if (vols.indexOf(letter) > -1) {
+//         newVal += 1;
+//       }
+
+//       return newVal;
+//     }, 0);
+// }
