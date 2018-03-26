57036899b5f36bdcd4b659b3d236a33b4a96076e
Ayush Goyal <ayush987goyal@gmail.com>
2018-03-26 14:13:50 +0530

    added fibnonacci with recursion and memoization

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,56 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn) {
+  const cache = {};
+
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    cache[args] = fn.apply(this, args);
+
+    return cache[args];
+  };
+}
+
+function slowFib(n) {
+  if (n <= 1) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+const fib = memoize(slowFib);
 
 module.exports = fib;
+
+// function fib(n) {
+//   const f = [];
+
+//   for (let i = 0; i <= n; i++) {
+//     if (i <= 1) {
+//       f[i] = i;
+//     } else {
+//       f[i] = f[i - 1] + f[i - 2];
+//     }
+//   }
+
+//   return f[n];
+// }
+
+// function fib(n) {
+//   let first = 0;
+//   let second = 1;
+
+//   for (let i = 2; i <= n; i++) {
+//     const temp = second;
+
+//     second = first + second;
+//     first = temp;
+//   }
+
+//   return second;
+// }
